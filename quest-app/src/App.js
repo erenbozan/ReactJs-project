0b013092025f6d1b685a49440fccc05244a74d8f
erenbozan <erenbozan@hotmail.com>
2024-08-13 20:39:13 +0300

    git try to new dev branch

--- quest-app/src/App.js
+++ quest-app/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import User from './components/User/User';
 
 function App() {
+  // The App component is the main component of the application. It is the parent component of all other components.
   return (
     <div className="App">
       <BrowserRouter>
